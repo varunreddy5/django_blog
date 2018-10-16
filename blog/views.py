0437f308f54f48d29fd08513fb2d1e90d73f01c4
Varun Reddy <varunreddy1061@gmail.com>
2018-10-16 07:56:44 -0400

    created database and migrated them

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-        {
-        'author': 'Varun 1',
-        'title': 'Blog Post 1',
-        'content' : 'First blog post content',
-        'date_posted' : 'October 1, 2018'
-        },
-        {
-        'author': 'Varun 2',
-        'title': 'Blog Post 2',
-        'content' : 'Second blog post content',
-        'date_posted' : 'October 2, 2018'
-        },
-
-]
+from .models import Post
 
 def home(request):
     context = {
-    'posts': posts
+    'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
