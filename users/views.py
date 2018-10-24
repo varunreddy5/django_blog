fe6887623bdc98a87b80a0612627d1221c83f088
Varun Reddy <varunreddy1061@gmail.com>
2018-10-23 20:54:36 -0400

    login and logout implementation

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
@@ -9,8 +10,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created successfully {username}')
-            return redirect('blog-home')
+            messages.success(request, f'You have successfully created an account. you can login now')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
