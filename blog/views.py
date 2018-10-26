ab104a6451dc02403f2c2b7e262ab557867b6a70
Varun Reddy <varunreddy1061@gmail.com>
2018-10-26 09:46:28 -0400

    Update and delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 # def home(request):
@@ -26,6 +26,33 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user #setting our author before validating
         return super().form_valid(form) #running the form_valid on parent
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    # success_url = '/'  # use this if you want to redirect it to home page
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user #setting our author before validating
+        return super().form_valid(form) #running the form_valid on parent
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
